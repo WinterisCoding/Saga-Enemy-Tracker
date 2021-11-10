1a05146a341318148df90b2d6b4c82a66eff8105
WinterisCoding <elisabethwinter.writes@gmail.com>
2021-11-09 21:22:57 -0500

    started building enemy class constructor

--- scripts/modules/enemies.js
+++ scripts/modules/enemies.js
@@ -12,4 +12,31 @@ const enemies = [
     }
 ]
 
+class enemy {
+    constructor() {
+        this.hp = 10;
+        this.speed = 30;
+        this.attacks = {
+            weapon: {
+                attack: 4,
+                damage: "3d8"
+            },
+            "Frag Grenade": {
+                attack: 3,
+                damage: "4d6, 2-square burst"
+            }
+        };
+        this.damage = "3d8";
+        this.threshold = 12;
+        this.refDefense = 16;
+        this.fortDefense = 12;
+        this.willDefense = 10;
+        this.special = "Coordinated Attack"
+        this.skills = {
+            Endurance: 7,
+            Perception: 9
+        };
+    }
+}
+
 export {enemies};
