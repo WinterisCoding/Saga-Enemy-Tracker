36a9a7e17e79752aeb9927e301f29c8885135d57
WinterisCoding <elisabethwinter.writes@gmail.com>
2021-11-09 20:17:46 -0500

    added another enemy

--- scripts/modules/enemies.js
+++ scripts/modules/enemies.js
@@ -1,8 +1,15 @@
 
 const enemies = [
-    {name: "stormtrooper",
+    {
+    name: "Stormtrooper",
     hp: 10,
-    attack: "+5"}
+    attack: "+5"
+    },
+    {
+    name: "Heavy Stormtrooper",
+    hp: 15,
+    attack: "+6"
+    }
 ]
 
 export {enemies};
