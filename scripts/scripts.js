457630d5ec05a6c4920252f1c3d0cd9d8146129d
WinterisCoding <elisabethwinter.writes@gmail.com>
2021-11-09 23:08:23 -0500

    added select menu with dynamic options

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,25 @@
 import {enemies} from "./modules/enemies.js"
 
-const init = () => {
-    console.log(enemies)
+const SWApp = {}
+
+SWApp.menu = document.getElementById('enemy');
+SWApp.enemies = enemies;
+
+// Function to display all the enemy options for the user to select
+SWApp.getOptions = () => {
+    SWApp.enemies.forEach((enemy) => {
+        console.log(enemy)
+        let option = document.createElement("option")
+        option.value = enemy.name;
+        option.innerText = enemy.name;
+        SWApp.menu.appendChild(option)
+    })
 }
 
-init()
+// function to initialize our app and call our first function
+SWApp.init = () => {
+    SWApp.getOptions();
+
+}
+
+SWApp.init()
