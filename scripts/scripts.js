81d947315c527f9186ba4a0757b641c10f0e9995
WinterisCoding <elisabethwinter.writes@gmail.com>
2021-11-10 23:26:16 -0500

    enemy element displaying on submit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,30 +8,42 @@ SWApp.enemies = enemies;
 
 // Function to display all the enemy options for the user to select
 SWApp.getOptions = () => {
-    SWApp.enemies.forEach((enemy) => {
+    SWApp.enemies.forEach((enemy, index) => {
         console.log(enemy)
         let option = document.createElement("option")
-        option.value = enemy.name;
+        option.value = index;
         option.innerText = enemy.name;
         SWApp.menu.appendChild(option)
     })
 }
 
-// Function to handle the submit event and add the enemy to the roster
+// Function to handle the submit event and add capture which enemy is selected
 SWApp.selectEnemy = () => {
     SWApp.formElement.addEventListener('submit', (e) => {
         e.preventDefault();
 
         let option = document.querySelector('select')
-        console.log(option.value)
+        SWApp.displayEnemy(option.value)
     })
+}
+
+// function to display the selected enemy
+SWApp.displayEnemy = (id) => {
+    let enemy = SWApp.enemies[id]
+    let enemyElement = document.createElement('li');
+
+    enemyElement.innerHTML = `
+    <h3>${enemy.name}</h3>
+        <p>Hit Points: ${enemy.hp}</p> 
+    `
+    let roster = document.querySelector('.enemyRoster')
+    roster.appendChild(enemyElement)
 }
 
 // function to initialize our app and call our first function
 SWApp.init = () => {
     SWApp.getOptions();
     SWApp.selectEnemy();
-
 }
 
 SWApp.init()
